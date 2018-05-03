58e1ba2dc07e397efc9798fe193febc97c9e2a7d
whycn <32027203+whycn@users.noreply.github.com>
2018-05-02 20:52:07 -0500

    complete hw8

---
+++ HW8/firmware/HW8.X/build/default/production/_ext/1360937237/LSM6DS333.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1360937237/LSM6DS333.o:  \
+../src/LSM6DS333.c  \
+ ../src/LSM6DS333.h 
---
+++ HW8/firmware/HW8.X/build/default/production/_ext/1360937237/main.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1360937237/main.o:  \
+../src/main.c  \
+ ../../../../../software/MPLAB\ Harmony/framework/system/common/sys_module.h  \
+ ../../../../../software/MPLAB\ Harmony/framework/system/common/sys_common.h 
